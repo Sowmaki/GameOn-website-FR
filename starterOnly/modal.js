582bf06efbec0e42f8401b21abb8416115939092
sowmaki <solene.tassel@hotmail.com>
2024-10-28 12:56:58 +0100

    ajout du message de validation, suppression du comportement par defaut du formulaire, ajout de message d'erreurs et style

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,15 +1,17 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  const myTopnav = document.getElementById("myTopnav");
+  if (myTopnav.className === "topnav") {
+    myTopnav.className += " responsive";
   } else {
-    x.className = "topnav";
+    myTopnav.className = "topnav";
   }
 }
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const closeBtn = document.querySelector(".btn-close");
+const closeCross = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
@@ -19,5 +21,55 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal event
+closeBtn.addEventListener("click", closeModal);
+closeCross.addEventListener("click", closeModal);
+
+// close modal event
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+/************ Form validation ************/
+
+//DOM Elements
+const myForm = document.getElementById("myForm");
+const divMerci = document.querySelector(".merci")
+
+
+// Form validation
+
+function validateForm(event) {
+  event.preventDefault();
+  let isFormValid = true;
+
+  // Error messages
+
+  formData.forEach((element) => {
+    const input = element.querySelector("input, select, textarea");
+    if (!input.checkValidity()) {
+      element.setAttribute("data-error-visible", "true");
+      isFormValid = false;
+    } else {
+      element.removeAttribute("data-error-visible");
+    }
+  });
+
+  // Form validation message
+
+  if (isFormValid) {
+    divMerci.style.display = "flex";
+    myForm.style.display = "none";
+  }
+}
+
+myForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  validateForm(event)
+})
+
+
+
 
 
